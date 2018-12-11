69cac6bb35940d8bc89a60bef5f045d74fc8d67c
Shemar Dacosta <dshemar6@gmail.com>
2018-12-11 07:53:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(dwarves)
+  d.each_with_index do |value, index|
+      o += " #{index.to_i+1}. #{value}"
+  end
+  puts "#{o}"
 end
 
 def summon_captain_planet# code an argument here
